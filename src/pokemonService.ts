76777dbbf8ebe4322ee2860985581ce0ac69f062
Andrew Jung <andrewdhjung@gmail.com>
2021-05-18 17:04:51 -0400

    Add fake mutation for POST example

--- src/pokemonService.ts
+++ src/pokemonService.ts
@@ -3,11 +3,22 @@ import { createApi, fetchBaseQuery } from '@rtk-incubator/rtk-query/react';
 export const pokemonApi = createApi({
     reducerPath: 'pokemonApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+    entityTypes: ["Pokemon"],
     endpoints: (builder) => ({
       getPokemonById: builder.query({
         query: (id: number) => `pokemon/${id}`,
       }),
+      createPokemon: builder.mutation({
+        query(body) {
+          return {
+            url: 'pokemon',
+            method: 'POST',
+            body
+          }
+        },
+        invalidates: ["Pokemon"]
+      })
     }),
   });
 
-  export const { useGetPokemonByIdQuery } = pokemonApi;
+  export const { useGetPokemonByIdQuery, useCreatePokemonMutation } = pokemonApi;
